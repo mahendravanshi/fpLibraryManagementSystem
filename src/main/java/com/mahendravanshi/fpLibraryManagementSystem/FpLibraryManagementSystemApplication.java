d8246cea3a47867347a868b7c1173c61a214b035
mahendravanshi <mahendrac121@gmail.com>
2025-06-08 00:26:30 +0530

    initial commit

---
+++ src/main/java/com/mahendravanshi/fpLibraryManagementSystem/FpLibraryManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.mahendravanshi.fpLibraryManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpLibraryManagementSystemApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/mahendravanshi/fpLibraryManagementSystem/FpLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahendravanshi.fpLibraryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
